8da5f979307c31d61c46f317c8edda498981601c
frank7841 <franklinewax@gmail.com>
2020-08-03 13:25:49 +0300

    Overiding onCreate Method

--- app/src/main/java/com/moringaschool/tronalddump/MainActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/MainActivity.java
@@ -3,12 +3,29 @@ package com.moringaschool.tronalddump;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.obama) Button mObama;
+    @BindView(R.id.bush) Button mBush;
+    @BindView(R.id.graham) Button mgraham;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mObama.setOnClickListener(this);
+        mBush.setOnClickListener(this);
+        mgraham.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+
     }
 }
