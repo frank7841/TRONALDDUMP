7f74499005edf9c556c878b2a09cba806465c090
frank7841 <franklinewax@gmail.com>
2020-08-03 15:28:43 +0300

    Overriding oncreate method

--- app/src/main/java/com/moringaschool/tronalddump/ObamaActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ObamaActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.tronalddump;
 
+import android.os.Bundle;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,15 @@ import butterknife.BindView;
 public class ObamaActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView mListView;
     public static final String TAG = ObamaActivity.class.getSimpleName();
+    private  String[] obama = new String[]{"Our great African American President " +
+            "hasn't exactly had a positive impact on the thugs who are so happily and" +
+            " openly destroying Baltimore! - 28 April 2015"
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obama);
 
 
 }
