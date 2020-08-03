9dc62a2c328c004ee82360f657b453e0790b3110
frank7841 <franklinewax@gmail.com>
2020-08-04 00:02:45 +0300

    Added List View

--- app/src/main/java/com/moringaschool/tronalddump/LindseyGrahamActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/LindseyGrahamActivity.java
@@ -1,8 +1,22 @@
 package com.moringaschool.tronalddump;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LindseyGrahamActivity extends AppCompatActivity {
+    @BindView(R.id.linseyGraham) TextView mLinseyGraham;
+    @BindView(R.id.listView)ListView mListView;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lindseygraham);
+        ButterKnife.bind(this);
 
+    }
 }
