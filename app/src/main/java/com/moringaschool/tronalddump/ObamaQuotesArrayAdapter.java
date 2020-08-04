537ad618a9a5e3e8cfc0f98c574440bd7f9d825a
frank7841 <franklinewax@gmail.com>
2020-08-04 08:04:25 +0300

    Finalising on the Project

--- app/src/main/java/com/moringaschool/tronalddump/ObamaQuotesArrayAdapter.java
+++ app/src/main/java/com/moringaschool/tronalddump/ObamaQuotesArrayAdapter.java
@@ -1,28 +1,28 @@
-package com.moringaschool.tronalddump;
-
-import android.content.Context;
-import android.widget.ArrayAdapter;
-
-import androidx.annotation.NonNull;
-
-public class ObamaQuotesArrayAdapter extends ArrayAdapter {
-    private Context mContext;
-    private String[] mObamaQuotes;
-
-    public ObamaQuotesArrayAdapter( Context mContext, int resource, String[] mObamaQuotes) {
-        super(mContext, resource);
-        this.mContext = mContext;
-        this.mObamaQuotes = mObamaQuotes;
-    }
-    @Override
-    public  Object getItem(int position){
-        String obamaQuote = mObamaQuotes[position];
-        return String.format("%s \nHe Said:%s", obamaQuote);
-    }
-    @Override
-    public int getCount(){
-        return mObamaQuotes.length;
-    }
-
-
-}
+//package com.moringaschool.tronalddump;
+//
+//import android.content.Context;
+//import android.widget.ArrayAdapter;
+//
+//import androidx.annotation.NonNull;
+//
+//public class ObamaQuotesArrayAdapter extends ArrayAdapter {
+//    private Context mContext;
+//    private String[] mObamaQuotes;
+//
+//    public ObamaQuotesArrayAdapter( Context mContext, int resource, String[] mObamaQuotes) {
+//        super(mContext, resource);
+//        this.mContext = mContext;
+//        this.mObamaQuotes = mObamaQuotes;
+//    }
+//    @Override
+//    public  Object getItem(int position){
+//        String obamaQuote = mObamaQuotes[position];
+//        return String.format("%s \nHe Said:%s", obamaQuote);
+//    }
+//    @Override
+//    public int getCount(){
+//        return mObamaQuotes.length;
+//    }
+//
+//
+//}
