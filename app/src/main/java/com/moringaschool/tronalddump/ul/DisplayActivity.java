2a08a4661d8e247ae5994d809d51ddbb712d1f77
frank7841 <franklinewax@gmail.com>
2020-08-13 00:16:58 +0300

    Created Display Activity class

---
+++ app/src/main/java/com/moringaschool/tronalddump/ul/DisplayActivity.java
@@ -0,0 +1,13 @@
+package com.moringaschool.tronalddump.ul;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
