4037d702ab234b4fb6ded8bb67bbe265ab79661c
frank7841 <franklinewax@gmail.com>
2020-08-12 19:52:09 +0300

    Implementing Interface TronaldDump

--- app/src/main/java/com/moringaschool/tronalddump/network/TronalDump.java
+++ app/src/main/java/com/moringaschool/tronalddump/network/TronalDump.java
@@ -1,4 +1,15 @@
 package com.moringaschool.tronalddump.network;
 
+import com.moringaschool.tronalddump.TronaldDump;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
 public interface TronalDump {
+    @Headers("accept: application/hal+json")
+    @GET("search/quote")
+    Call<TronaldDump> searchQuote(
+            @Query("Query") String Search);
 }
