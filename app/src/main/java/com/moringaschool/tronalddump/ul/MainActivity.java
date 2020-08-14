3d3926bff6714fb9498c5a3908c3952deadfde5a
frank7841 <franklinewax@gmail.com>
2020-08-14 20:30:31 +0300

    Implementing onClick method

--- app/src/main/java/com/moringaschool/tronalddump/ul/MainActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ul/MainActivity.java
@@ -20,7 +20,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
     @BindView(R.id.textView2) TextView mTexview2;
     @BindView(R.id.nextbutton) Button mButton;
 
@@ -29,7 +29,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-    }
-   
 
+        mButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent memepage= new Intent(MainActivity.this,DisplayActivity.class);
+        startActivity(memepage);
+    }
 }
