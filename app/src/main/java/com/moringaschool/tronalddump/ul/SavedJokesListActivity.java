c59ab0f826cbe2467123c5a2d05e9a2e9486ff49
frank7841 <franklinewax@gmail.com>
2020-08-19 12:50:40 +0300

    Creating savedjokesListActivity

---
+++ app/src/main/java/com/moringaschool/tronalddump/ul/SavedJokesListActivity.java
@@ -0,0 +1,12 @@
+package com.moringaschool.tronalddump.ul;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedJokesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
