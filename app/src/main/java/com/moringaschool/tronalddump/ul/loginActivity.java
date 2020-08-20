ef551fec31331ed76334b81fecbd936fbc937188
frank7841 <franklinewax@gmail.com>
2020-08-20 07:37:59 +0300

    formatting Display

--- app/src/main/java/com/moringaschool/tronalddump/ul/loginActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ul/loginActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.tronalddump.ul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import com.moringaschool.tronalddump.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class loginActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.registerTextView) TextView mregisterTextView;
@@ -17,10 +19,17 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mregisterTextView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        if (view==mregisterTextView){
+            Intent intent = new Intent(loginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
