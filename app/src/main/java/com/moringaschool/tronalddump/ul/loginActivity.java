4d146050b01327bc38406725b5377c2dfddb0951
frank7841 <franklinewax@gmail.com>
2020-08-20 06:18:35 +0300

    Created login Activity

---
+++ app/src/main/java/com/moringaschool/tronalddump/ul/loginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.tronalddump.ul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.tronalddump.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
