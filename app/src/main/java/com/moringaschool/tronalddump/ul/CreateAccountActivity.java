79b61bf421cd2d026ae0055cd6ce561bf75a567a
frank7841 <franklinewax@gmail.com>
2020-08-20 08:43:09 +0300

    Implementing New USer

--- app/src/main/java/com/moringaschool/tronalddump/ul/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ul/CreateAccountActivity.java
@@ -2,18 +2,22 @@ package com.moringaschool.tronalddump.ul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.tronalddump.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
     @BindView(R.id.createUserButton) Button mCreateUserButton;
     @BindView(R.id.nameEditText) EditText mEditText;
     @BindView(R.id.emailEditText) EditText mEmailEditText;
@@ -26,14 +30,26 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
+        mAuth=FirebaseAuth.getInstance();
 
         mLoginTextView.setOnClickListener(this);// redirect to login
         mCreateUserButton.setOnClickListener(this);//submit new user
 
     }
+    public void createNewUser(){
+
+    }
 
     @Override
     public void onClick(View view) {
-
+        if (view==mLoginTextView){
+            Intent intent= new Intent(CreateAccountActivity.this, loginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view==mCreateUserButton){
+            createNewUser();
+        }
     }
 }
