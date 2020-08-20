fe81deb2fe92b863c8b222a028a009a0b7ae5fcd
frank7841 <franklinewax@gmail.com>
2020-08-20 07:21:04 +0300

    Redirecting to Create Account Page

--- app/src/main/java/com/moringaschool/tronalddump/ul/loginActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ul/loginActivity.java
@@ -3,14 +3,24 @@ package com.moringaschool.tronalddump.ul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.tronalddump.R;
 
-public class loginActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class loginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mregisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
