942a82bac1aad93e0a053bc13a7d6e648686e6d3
frank7841 <franklinewax@gmail.com>
2020-08-20 08:11:24 +0300

    Declaring varriables for Signup

--- app/src/main/java/com/moringaschool/tronalddump/ul/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/tronalddump/ul/CreateAccountActivity.java
@@ -3,14 +3,37 @@ package com.moringaschool.tronalddump.ul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.tronalddump.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.nameEditText) EditText mEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);// redirect to login
+        mCreateUserButton.setOnClickListener(this);//submit new user
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
